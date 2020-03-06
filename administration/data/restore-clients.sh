14d21488c2458a7b62a49044ae365a9a3696fe22
Doug M. Harris <dharris@hq.xsilo.com>
2020-03-06 18:21:04 -0500

    Fix bucket name and key expression

--- administration/data/restore-clients.sh
+++ administration/data/restore-clients.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/opt/couchbase/bin/cbimport json -c couchbase://localhost -u $1 -p $2 -b gluu_user -f list -g "%clients_%inum%" -d file://$3
+/opt/couchbase/bin/cbimport json -c couchbase://localhost -u $1 -p $2 -b gluu -f list -g "clients_%inum%" -d file://$3
 
