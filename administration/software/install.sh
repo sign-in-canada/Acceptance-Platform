b8fed5e0aff96fc854c57ca71defdc11ad5d12ca
Doug M. Harris <dharris@hq.xsilo.com>
2020-05-06 16:56:36 -0400

    Support restoration of passport providers

--- administration/software/install.sh
+++ administration/software/install.sh
@@ -37,20 +37,28 @@ echo "Configuring SAML metadata URL..."
 sed -i "s|\[URL\]|${METADATA_URL}|g" \
    /opt/gluu-server/opt/dist/signincanada/shibboleth-idp/conf/metadata-providers.xml
 
+if [ -f ./passport-central-config.json ] ; then
+   echo "Restoring CSP and IDP configurations"
+   cat ./passport-central-config.json > /opt/gluu-server/install/community-edition-setup/templates/passport-central-config.json
+fi
+
 echo "Configuring Gluu..."
 cp setup.properties.last.enc /opt/gluu-server/install/community-edition-setup/setup.properties.enc
 ssh  -o IdentityFile=/etc/gluu/keys/gluu-console -o Port=60022 -o LogLevel=QUIET \
                 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
                 -o PubkeyAuthentication=yes root@localhost \
-   "/install/community-edition-setup/setup.py -n -f /install/community-edition-setup/setup.properties.enc -properties-password $PASSWORD ; \
+   "/install/community-edition-setup/setup.py -n -f /install/community-edition-setup/setup.properties.enc -properties-password $PASSWORD --import-ldif=/opt/dist/signincanada/ldif ; \
     /opt/dist/signincanada/postinstall.sh"
 
 if [ -f ./oxauth-keys.jks ] ; then
    echo "Restoring the oxAuth keystore."
-   cp /opt/gluu-server/etc/certs/oxauth-keys.jks .
+   cat ./oxauth-keys.jks > /opt/gluu-server/etc/certs/oxauth-keys.jks
 fi
 
 echo "Restarting..."
 /sbin/gluu-serverd restart
+
+echo "Cleaning up..."
+rm -f ${1}.tgz ${1}.tgz.sha
 
 echo "${1} has been installed."
