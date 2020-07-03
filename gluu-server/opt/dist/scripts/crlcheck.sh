e550ad5059f33aa180a70670b465ca346e467e9f
Doug M. Harris <dharris@hq.xsilo.com>
2020-07-03 10:30:12 -0400

    Debug crl script

--- gluu-server/opt/dist/scripts/crlcheck.sh
+++ gluu-server/opt/dist/scripts/crlcheck.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-ICM_ROOT="/etc/certs/icm-root.pem"
-GGCKEY_CERT="/etc/certs/gckey-signing.pem"
+ICM_ROOT="/opt/dist/certs/icm-root.pem"
+GCKEY_CERT="/opt/dist/certs/gckey-signing.pem"
 GCKEY_CRL="ldap://ldap.gss-spg.gc.ca:389/cn=CRL3995,ou=1CA-AC1,ou=GSS-SPG,o=GC,c=CA"
-CBS_CERT="/etc/certs/cbs-signing.pem"
-CBS_CRL="ldap://ldap.gss-spg.gc.ca:389/cn=CRL3995,ou=1CA-AC1,ou=GSS-SPG,o=GC,c=CA"
+CBS_CERT="/opt/dist/certs/cbs-signing.pem"
+CBS_CRL="ldap://ldap.gss-spg.gc.ca:389/cn=CRL4740,ou=1CA-AC1,ou=GSS-SPG,o=GC,c=CA"
 
 cd /tmp
 umask 77
 
 # Downnload the latest CRLs for GCKey and CBS; exit if ICM does not respond
-curl $GCKEY_CRL > gckey.ldap || exit
-curl $CBS_CRL > cbs.ldap || exit
-
+curl -s $GCKEY_CRL > GCKey.ldap || exit
+curl -s $CBS_CRL > CBS.ldap || exit
 
 # Extract the CRLs and convert to PEM
-grep -Po '(?<=certificateRevocationList:: )(.+)' GCKey.ldap | \
+grep -Po '(?<=certificateRevocationList;binary:: )(.+)' GCKey.ldap | \
    base64 -d | \
    openssl crl -inform DER -outform PEM -out gckey-crl.pem
-grep -Po '(?<=certificateRevocationList:: )(.+)' CBS.ldap | \
+grep -Po '(?<=certificateRevocationList;binary:: )(.+)' CBS.ldap | \
    base64 -d | \
    openssl crl -inform DER -outform PEM -out cbs-crl.pem
 
 # Verify CRL integrity; exit if failed
-openssl crl crl -inform DER -CAFile $ICM_ROOT -noout -in gckey-crl.pem || exit
-openssl crl crl -inform DER -CAFile $ICM_ROOT -noout -in cbs-crl.pem || exit
+openssl crl -inform PEM -CAfile $ICM_ROOT -noout -in gckey-crl.pem || exit
+openssl crl -inform PEM -CAfile $ICM_ROOT -noout -in cbs-crl.pem || exit
 
-# Check the revocation status
-if [ openssl verify -CAFile $ICM_ROOT -CRLFile gckey-crl.pem $GCKEY_CERT && 
-     openssl verify -CAFile $ICM_ROOT -CRLFile cbs-crl.pem $CBS_CERT ] ; then
-     echo "All Good"
-else
-     echo "a CSP certificate has been revoked"
-
+# Check the revocation status; shutdown passport if either is revoked
+openssl verify -CAfile $ICM_ROOT -crl_check -CRLfile gckey-crl.pem $GCKEY_CERT || systemctl stop passport
+openssl verify -CAfile $ICM_ROOT -crl_check -CRLfile cbs-crl.pem $CBS_CERT  || systemctl stop passport
