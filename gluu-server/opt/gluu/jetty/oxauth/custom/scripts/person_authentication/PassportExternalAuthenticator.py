b524d73365a13892277b21ab1441a3c0ead4e7a6
Doug M. Harris <dharris@hq.xsilo.com>
2020-10-09 09:46:36 -0400

    Update EncryptionService package
    It's been moved in 4.2

--- gluu-server/opt/gluu/jetty/oxauth/custom/scripts/person_authentication/PassportExternalAuthenticator.py
+++ gluu-server/opt/gluu/jetty/oxauth/custom/scripts/person_authentication/PassportExternalAuthenticator.py
@@ -12,7 +12,8 @@ from org.gluu.oxauth.model.configuration import AppConfiguration
 from org.gluu.oxauth.model.crypto import CryptoProviderFactory
 from org.gluu.oxauth.model.jwt import Jwt, JwtClaimName
 from org.gluu.oxauth.model.util import Base64Util
-from org.gluu.oxauth.service import AppInitializer, AuthenticationService, UserService, SessionIdService, EncryptionService
+from org.gluu.oxauth.service import AppInitializer, AuthenticationService, UserService, SessionIdService
+from org.gluu.oxauth.service.common import EncryptionService
 from org.gluu.oxauth.service.net import HttpService
 from org.gluu.oxauth.security import Identity
 from org.gluu.oxauth.util import ServerUtil
