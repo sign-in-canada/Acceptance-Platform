f068f34404256a0c91180d0e6b5f6b1e89cc4e9a
Naeem UL-Haq <naeemu@macbook-pro.lan>
2020-11-24 20:30:15 -0500

    use of pre-loaded certs

--- gluu-server/opt/dist/scripts/azure/keyvault.sh
+++ gluu-server/opt/dist/scripts/azure/keyvault.sh
@@ -9,6 +9,7 @@
 KEYVAULT=$1
 API_VER='7.0'
 KV_DIR=/run/keyvault
+certname_prefix=$(cat ${KV_DIR}/certs/hostname)
 
 extractJSONValue () {
    /opt/node/bin/node -e "console.log(JSON.parse(require('fs').readFileSync(0, 'utf8'))[process.argv[1]])" $1
@@ -32,28 +33,18 @@ fi
 
 # Create a ramfs directory to hold the secrets
 umask 227
-mkdir $KV_DIR
 mount -t ramfs ramfs $KV_DIR
-mkdir ${KV_DIR}/certs ${KV_DIR}/secrets
+${KV_DIR}/secrets
 
 # Get the certificates and their private keys
-certs=$(curl -s -H "Authorization: Bearer ${TOKEN}" ${KEYVAULT}/certificates?api-version=${API_VER} \
-   | /opt/node/bin/node -e \
-    "let certs = JSON.parse(require('fs').readFileSync(0, 'utf8')).value; \
-     for (cert of certs) { \
-        console.log(cert.id.split('/').pop()); \
-     }" )
+#openssl x509 -outform pem  > ${KV_DIR}/certs/$certname_prefix.crt
+#openssl rsa -outform PEM > ${KV_DIR}/certs/$certname_prefix.key
+#sed '1,/-----END CERTIFICATE-----/d' > ${KV_DIR}/certs/${cert}.chain
+   
+ln -s -f ${KV_DIR}/certs/$certname_prefix.crt /etc/certs/$certname_prefix.crt
+ln -s -f ${KV_DIR}/certs/fullchain.cer /etc/certs/fullchain.cer
+ln -s -f ${KV_DIR}/certs/$certname_prefix.key /etc/certs/$certname_prefix.key
 
-for cert in $certs ; do
-   fetchSecret $cert | tee \
-      >(openssl x509 -outform pem  > ${KV_DIR}/certs/${cert}.crt) \
-      >(openssl rsa -outform PEM > ${KV_DIR}/certs/${cert}.key) \
-      >(sed '1,/-----END CERTIFICATE-----/d' > ${KV_DIR}/certs/${cert}.chain) \
-      > /dev/null
-   ln -s -f ${KV_DIR}/certs/${cert}.crt /etc/certs/${cert}.crt
-   ln -s -f ${KV_DIR}/certs/${cert}.chain /etc/certs/${cert}.chain
-   ln -s -f ${KV_DIR}/certs/${cert}.key /etc/certs/${cert}.key
-done
 
 # Get the Application Insights Instrumentation Key
 fetchSecret InstrumentationKey > ${KV_DIR}/secrets/InstrumentationKey
