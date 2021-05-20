3367e9d914c599f7aea408a6e76611dbd2131d80
Doug Harris <doug.harris@xsilosystems.com>
2021-05-20 12:42:52 -0400

    Support the new RP feature configuration method

--- gluu-server/opt/gluu/jetty/oxauth/custom/scripts/dynamic_scope/sic_saml_nameid.py
+++ gluu-server/opt/gluu/jetty/oxauth/custom/scripts/dynamic_scope/sic_saml_nameid.py
@@ -5,51 +5,54 @@
 #
 
 from org.gluu.model.custom.script.type.scope import DynamicScopeType
-from org.gluu.oxauth.service import UserService
-from org.gluu.util import StringHelper, ArrayHelper
-from java.util import Arrays, ArrayList
+from org.gluu.service.cdi.util import CdiUtil
+from org.gluu.oxauth.service import ClientService
+from org.gluu.util import StringHelper
+from java.util import Arrays
 from org.json import JSONObject
 
-import java
+import sys, json
 
 class DynamicScope(DynamicScopeType):
     def __init__(self, currentTimeMillis):
         self.currentTimeMillis = currentTimeMillis
 
-    def init(self, configurationAttributes):
-        print "Dynamic scope [saml_nameid_scope]. Initialization"
+    def init(self, customScript, configurationAttributes):
+        print ("Dynamic scope [saml_nameid_scope]. Initialization")
+        # Keep an in-memory cache of RP Configs
+        self.rpConfigCache = {}
 
         return True   
 
     def destroy(self, configurationAttributes):
-        print "Dynamic scope [saml_nameid_scope]. Destroy"
+        print ("Dynamic scope [saml_nameid_scope]. Destroy")
         return True   
 
     # Update Json Web token before signing/encrypring it
     #   dynamicScopeContext is org.gluu.oxauth.service.external.context.DynamicScopeExternalContext
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def update(self, dynamicScopeContext, configurationAttributes):
-        print "Dynamic scope [saml_nameid_scope]. Update method"
+        print ("Dynamic scope [saml_nameid_scope]. Update method")
 
         # Get the client and SAML affilitation value
         authorizationGrant = dynamicScopeContext.getAuthorizationGrant()
-        oidcClient = authorizationGrant.getClient()
-        samlSpNameQualifier = oidcClient.getPolicyUri()
+        rpConfig = self.getRPConfig(authorizationGrant.getClient())
+        samlSpNameQualifier = rpConfig.get("collect")
         
         # if samlSpNameQualifier is not empty, we pass the affiliated SAML nameid
         if ( samlSpNameQualifier != None ):
             # then we look for the SAML persistentId value in user profile
-            print "Dynamic scope [saml_nameid_scope]. Found SPNameQualifier parameter '%s'" % samlSpNameQualifier
+            print ("Dynamic scope [saml_nameid_scope]. Found SPNameQualifier parameter '%s'" % samlSpNameQualifier)
             user = dynamicScopeContext.getUser()
             userPersistentIds = user.getAttributeValues("persistentId")
-            print "Dynamic scope [saml_nameid_scope]. Found SPNameQualifier parameter"
+            print ("Dynamic scope [saml_nameid_scope]. Found SPNameQualifier parameter")
             if ( userPersistentIds != None ):
                 if ( userPersistentIds.size > 0 ):
                     # go through existing user persistentIds
                     for userPersistentId in userPersistentIds:
                         # if the current RP already has a mapping then skip the second phase
                         if ( userPersistentId.find(samlSpNameQualifier) > -1 ):
-                            print "Dynamic scope [saml_nameid_scope]. Found matching persistentId '%s'" % userPersistentId
+                            print ("Dynamic scope [saml_nameid_scope]. Found matching persistentId '%s'" % userPersistentId)
                             # Format is : persistentIdSamlSpNQ|persistentIdIdp|persistentIdUid
                             samlSpNameQualifier  = StringHelper.split(userPersistentId,'|')[0]
                             samlIDPNameQualifier = StringHelper.split(userPersistentId,'|')[1]
@@ -65,8 +68,37 @@ class DynamicScope(DynamicScopeType):
         return True
 
     def getSupportedClaims(self, configurationAttributes):
-        print "Dynamic scope [saml_nameid_scope]. Get supported claims = 'saml_nameid'"
+        print ("Dynamic scope [saml_nameid_scope]. Get supported claims = 'saml_nameid'")
         return Arrays.asList("saml_nameid")
 
     def getApiVersion(self):
-        return 2
+        return 11
+
+    def getRPConfig(self, client):
+        clientService = CdiUtil.bean(ClientService)
+
+        # check the cache
+        clientKey = "oidc:%s" % client.getClientId()
+        if clientKey in self.rpConfigCache:
+            return self.rpConfigCache[clientKey]
+
+        descriptionAttr = clientService.getCustomAttribute(client, "description")
+
+        rpConfig = None
+        if descriptionAttr is not None:
+            description = descriptionAttr.getValue()
+            start = description.find("{")
+            if (start > -1):
+                decoder = json.JSONDecoder()
+                try:
+                    rpConfig, _ = decoder.raw_decode(description[start:])
+                except ValueError:
+                    print ("%s. getRPConfig: Failed to parse JSON config for client %s" % (self.name, client.getClientName()))
+                    print ("Exception: ", sys.exc_info()[1])
+
+        if rpConfig is None:
+            rpConfig = {}
+
+        # Add it to the cache
+        self.rpConfigCache[clientKey] = rpConfig
+        return rpConfig
