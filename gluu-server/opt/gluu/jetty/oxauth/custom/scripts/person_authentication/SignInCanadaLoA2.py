e1f6715020abc9bb4282b15eefea6c7bd9d01e77
Doug Harris <doug.harris@xsilosystems.com>
2021-05-21 14:10:21 -0400

    Check for existing ids when collecting
    This should not normally happen, but sometimes
    thongs don't happen normally.

--- gluu-server/opt/gluu/jetty/oxauth/custom/scripts/person_authentication/SignInCanadaLoA2.py
+++ gluu-server/opt/gluu/jetty/oxauth/custom/scripts/person_authentication/SignInCanadaLoA2.py
@@ -395,11 +395,14 @@ class PersonAuthentication(PersonAuthenticationType):
                 spNameQualifier = rpConfig["collect"]
             if nameQualifier == "undefined":
                 nameQualifier = externalProfile["issuer"][0]
-            user = self.account.addSamlSubject(user, spNameQualifier, nameQualifier, nameId)
+            if not self.account.getSamlSubject(user, spNameQualifier): # unless one already exists
+                user = self.account.addSamlSubject(user, spNameQualifier, nameQualifier, nameId)
             userService.updateUser(user)
 
             # construct an OIDC pairwisae subect using the SAML PAI
-            self.account.addOpenIdSubject(user, self.getClient(session), provider + nameId)
+            client = self.getClient(session)
+            if not self.account.getOpenIdSubject(user, client): # unless one already exists
+                self.account.addOpenIdSubject(user, client, provider + nameId)
 
             # Is MFA required next?
             if rpConfig.get("mfa"):
