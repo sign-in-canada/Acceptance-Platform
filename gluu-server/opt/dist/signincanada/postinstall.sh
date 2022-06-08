9c8188c886f4c853aebcf1a18bd1fbe8a1edf01b
Doug Harris <doug.harris@xsilosystems.com>
2022-06-08 09:43:31 -0400

    Fix typo

--- gluu-server/opt/dist/signincanada/postinstall.sh
+++ gluu-server/opt/dist/signincanada/postinstall.sh
@@ -18,7 +18,7 @@ systemctl enable keyvault
 echo 'Installing logstash...'
 rpm --import /etc/pki/rpm-gpg/GPG-KEY-elasticsearch
 yum install -y /opt/dist/app/logstash-*-x86_64.rpm
-/usr/share/logsash/bin/logstash-plugin install file:///opt/dist/app/logstash-offline-plugins-8.2.2.zip
+/usr/share/logstash/bin/logstash-plugin install file:///opt/dist/app/logstash-offline-plugins-8.2.2.zip
 
 echo 'Enabling the couchbase health check service...'
 systemctl enable cbcheck
