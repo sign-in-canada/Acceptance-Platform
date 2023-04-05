4c21f5322e196ba366479d0b5335656bcaeec683
Doug Harris <doug.harris@xsilosystems.com>
2023-04-05 14:59:28 -0400

    Remove logstash offline plugin builder

--- pipeline-utils/logstash/offline-plugins.sh
+++
@@ -1,2 +0,0 @@
-bin/logstash-plugin install logstash-filter-json_encode microsoft-logstash-output-azure-loganalytics
-bin/logstash-plugin prepare-offline-pack --output /srv/logstash/logstash-offline-plugins-8.6.1.zip logstash-filter-json_encode microsoft-logstash-output-azure-loganalytics
